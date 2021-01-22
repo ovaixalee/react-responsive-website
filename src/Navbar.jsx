f0b4887d28d5de51002f8f0f48086b1b1d3799df
Owais Ali <54909773+ovaixalee@users.noreply.github.com>
2021-01-22 11:04:56 +0100

    update services text

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -51,7 +51,7 @@ const Navbar = () =>
                                     activeClassName="menu_active" 
                                     className="nav-link" 
                                     to="/services#services">
-                                    Services
+                                    Unsere Services
                                 </NavLink>
                             </li>
                             <li className="nav-item">
