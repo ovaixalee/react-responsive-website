4d4ae7d09046635187eb0b4f6397a4694f017e2f
Owais Ali <54909773+ovaixalee@users.noreply.github.com>
2021-01-28 09:00:17 +0100

    uploading website

---
+++ src/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import coverimg from "./images/coverimg.png"
+import Common from './Common'
+
+const Home = () =>
+    <>
+    <Common 
+    name="Steigern Sie Ihr Geschäft mit"
+    text="Wir bringen Ihre Vorstellungen, Wünsche und Ressourcen in Einklang mit den technischen Möglichkeiten der heutigen Zeit."
+    imgsrc={coverimg}
+    visit="/service"
+    btname="Anfragen"
+    />
+    
+ 
+
+    </>
+
+
+export default Home
